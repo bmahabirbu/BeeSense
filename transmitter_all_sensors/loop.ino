087bbd273d3a8ce860623cdfdfceffd2ece669b4
Brian M <56164556+bmahabirbu@users.noreply.github.com>
2021-11-14 05:07:31 -0500

    Add files via upload
    Code for feather m0 and Raspi

---
+++ transmitter_all_sensors/loop.ino
@@ -0,0 +1,40 @@
+void loop() {
+
+  print_temp();
+  print_rtc();
+  print_ir();
+  print_gas();
+  //Fake mic code
+  randNumber = random(1000,1500);
+  Serial.print("Fake Mic val: ");
+  Serial.println(randNumber);
+
+  String temp_string = "TemperatureC*: ";
+  String temp_num = String(PCT2075.getTemperature(), 2);
+  String temp_message = temp_string+temp_num;
+
+  DateTime now = rtc.now();
+  String date_year = String(now.year(), DEC);
+  String date_month = String(now.month(), DEC);
+  String date_day = String(now.day(), DEC);
+  String date_hour = String(now.hour(), DEC);
+  String date_min = String(now.minute(), DEC);
+  String date_sec = String(now.second(), DEC);
+
+  String date = "Date now: "+date_year+"/"+date_month+"/"+date_day+"/"+date_hour+":"+date_min+":"+date_sec;
+
+  String pixel_str = String(pixels[0], 2);
+  String ir_str = "First IR pixel temp: "+pixel_str;
+
+  String eco2_str = String(sgp.eCO2);
+  String gas_str = "eCo2: "+eco2_str+" ppm";
+
+  String noise_str = String(randNumber);
+  String mic_str = "Fake mic noise: "+noise_str;
+
+  String msg = temp_message+" "+date+" "+ir_str+" "+gas_str+" "+mic_str;
+  send_message(msg);
+
+  delay(3000);
+    
+}
