88b12232c6846eda1a51135b1de8ee36183f1b57
BUSolarLab <bmahabir@bu.edu>
2022-01-26 00:11:20 -0500

    Testing SD card and Lora working together by switching cs pin

--- Under_development/SD_card_test/SD_card_test.ino
+++ Under_development/SD_card_test/SD_card_test.ino
@@ -9,7 +9,7 @@
  ** MOSI - pin 11
  ** MISO - pin 12
  ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+ ** CS - pin 8 fpr adsamd21
 
   created   Nov 2010
   by David A. Mellis
@@ -23,18 +23,33 @@
 
   add lora to test switching capabilities
 
+  //Set pin 8 high for use with SD card 
+  //Set pin 8 low after finished with the sd card
+
   
 
 */
 #include <SPI.h>
 #include <SD.h>
+#include <RH_RF95.h>
+
+//for feather m0  
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 905.5
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 File root;
 
 void setup() {
-  //Set pin 8 high for use with SD card 
   pinMode(8, OUTPUT);
-  digitalWrite(8, HIGH);
+  lora_switch(false);
+   
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -49,15 +64,32 @@ void setup() {
   }
   Serial.println("initialization done.");
 
-  root = SD.open("/");
-
-  printDirectory(root, 0);
-
-  Serial.println("done!");
+  //switch cs pin
+  lora_switch(true);
+  
+  //setup lora
+  setup_lora();
+  
+  //switch cs pin
+  lora_switch(false);
+   
 }
 
 void loop() {
-  // nothing happens after setup finishes.
+  
+  //print sd card
+  root = SD.open("/");
+  printDirectory(root, 0);
+  Serial.println("done!");
+  //switch cs pin
+  lora_switch(true);
+  
+  //send message
+  String msg = "Hellow world";
+  send_message(msg);
+  
+  //switch cs pin
+  lora_switch(false);
 }
 
 void printDirectory(File dir, int numTabs) {
@@ -81,5 +113,74 @@ void printDirectory(File dir, int numTabs) {
       Serial.println(entry.size(), DEC);
     }
     entry.close();
+  }
+}
+
+
+void setup_lora(){
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+
+  delay(100);
+
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+
+void send_message(String string){
+  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
+  digitalWrite(13, HIGH); //LED high
+  Serial.println("Transmitting..."); // Send a message to rf95_server
+  String radiopacket = string;
+  Serial.println("Sending...");
+  Serial.println(string);
+  delay(10);
+  rf95.send((uint8_t *)radiopacket.c_str(),radiopacket.length());
+
+  Serial.println("Waiting for packet to complete..."); 
+  delay(10);
+  rf95.waitPacketSent();
+  digitalWrite(13, LOW);//LED off
+}
+
+void lora_switch(bool flag) {
+  if(flag == true){
+    digitalWrite(8, LOW);
+    delay(10);
+  }
+  else {
+    digitalWrite(8, HIGH);
+    delay(10);
   }
 }
